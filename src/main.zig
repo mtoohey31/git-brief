143550f5cee0de01b0f06d4ab542044ce6f8c849
Matthew Toohey <contact@mtoohey.com>
2022-07-31 17:49:04 -0400

    refactor: general cleanup

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,11 @@ const git = @cImport({
     @cInclude("git2/repository.h");
 });
 
+const DEBUG_LEAKS = false;
+
 const stdio = @cImport(@cInclude("stdio.h"));
 const std = @import("std");
 const mode = @import("builtin").mode;
-
-// using the absolute path is necessary because the user presumeably has this
-// program set up as a wrapper for git, so if we use $PATH, we're likely to
-// recursively execute ourself endlessly
-const git_path = "/usr/bin/git";
 
 // TODO: clean up logging so it looks prettier and declares that it's from
 // git-brief, so people don't think errors are coming from git, and don't fail
@@ -21,93 +18,136 @@ const git_path = "/usr/bin/git";
 // TODO: understand aliases and arguments better. for example: ignore
 // unimportant whitespce
 
+fn find_git(allocator: std.mem.Allocator, arg0: []const u8) !?[*:0]const u8 {
+    const arg0_dir = std.fs.path.dirname(arg0);
+    var path_iterator = std.mem.split(u8, std.os.getenv("PATH").?, ":");
+    while (path_iterator.next()) |entry| {
+        // if the entry is in the same directory as the binary being executed,
+        // skip it because we don't want to re-execute ourself
+        if (std.mem.eql(u8, arg0_dir.?, entry)) {
+            continue;
+        }
+
+        var git_path = try std.fs.path.joinZ(allocator, &[_][]const u8{ entry, "git" });
+        var stat: std.os.Stat = undefined;
+        // TODO: figure out why std.os.errno is broken
+        const r = @bitCast(isize, std.os.linux.stat(git_path, &stat));
+        if (r == 0) {
+            return git_path;
+        } else if (r != -2) {
+            return error.StatFailed;
+        }
+        allocator.free(git_path);
+    }
+    return null;
+}
+
+const ConfigIterator = struct {
+    config: ?*git.git_config,
+    inner: ?*git.git_config_iterator,
+
+    fn init(glob: [*c]const u8) !ConfigIterator {
+        // TODO: figure out if there's a way to get friendly errors from libgit2
+        if (git.git_libgit2_init() < 0) {
+            return error.LibGit2Failed;
+        }
+
+        var self = ConfigIterator{ .config = undefined, .inner = undefined };
+
+        if (git.git_config_open_default(&self.config) < 0) {
+            return error.LibGit2Failed;
+        }
+
+        if (git.git_config_iterator_glob_new(&self.inner, self.config, glob) < 0) {
+            return error.LibGit2Failed;
+        }
+
+        return self;
+    }
+
+    fn deinit(self: *ConfigIterator) void {
+        git.git_config_iterator_free(self.inner);
+        git.git_config_free(self.config);
+
+        // "resource deallocation must succeed"
+        _ = git.git_libgit2_shutdown();
+    }
+
+    fn next(self: *ConfigIterator) !?*git.git_config_entry {
+        var config_entry: ?*git.git_config_entry = undefined;
+        const res = git.git_config_next(&config_entry, self.inner);
+        if (res < 0) {
+            // means we've read all the aliases
+            if (res == git.GIT_ITEROVER) {
+                return null;
+            }
+
+            // TODO: figure out what the error is and wrap it
+            return error.LibGit2Failed;
+        }
+        return config_entry;
+    }
+};
+
 pub fn main() anyerror!void {
-    // initialize allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    // an arraylist to hold all the 
-    var argv: std.ArrayList(?[*:0]const u8) = undefined;
+    // an arraylist to hold all the command line args
+    var argv = std.ArrayList(?[*:0]const u8).init(allocator);
 
-    // this all happens at a separate level so deinit defers in here happen
-    // before leak checks
-    {
-        argv = std.ArrayList(?[*:0]const u8).init(allocator);
-        // an arraylist to hold the args, concatenated by ' '
-        var args_concat = std.ArrayList(u8).init(allocator);
-        defer args_concat.deinit();
-        var args = try std.process.argsWithAllocator(allocator);
-        if (args.next()) |a| {
-            try argv.append(a);
-        }
-        if (args.next()) |a| {
-            try argv.append(a);
-            try args_concat.appendSlice(a);
-        }
-        while (args.next()) |a| {
-            try argv.append(a);
+    var args = try std.process.argsWithAllocator(allocator);
+    while (args.next()) |a| {
+        try argv.append(a);
+    }
+    args.deinit();
+
+    // args concatenated by ' '
+    var args_concat = std.ArrayList(u8).init(allocator);
+
+    for (argv.items[1..]) |rarg| {
+        if (rarg) |arg| {
+            try args_concat.appendSlice(std.mem.span(arg));
             try args_concat.append(' ');
-            try args_concat.appendSlice(a);
         }
-        args.deinit();
+    }
+    // remove the last space
+    _ = args_concat.popOrNull();
 
-        // reading the config
-        var cfg: ?*git.git_config = undefined;
+    // the path to the git binary that will be executed
+    const git_path = (try find_git(allocator, std.mem.span(argv.items[0].?))) orelse "/usr/bin/git";
 
-        // TODO: figure out if there's a way to get libgit2 to print friendly
-        // errors
-        if (git.git_libgit2_init() < 0) {
-            std.log.err("failed to initialize libgit2", .{});
-            std.process.exit(1);
-        }
-
-        if (git.git_config_open_default(&cfg) < 0) {
-            std.log.err("failed to open git configuration", .{});
-        }
-
-        var iter: ?*git.git_config_iterator = undefined;
-        defer git.git_config_iterator_free(iter);
-        if (git.git_config_iterator_glob_new(&iter, cfg, "^alias\\.") < 0) {
-            std.log.err("failed to initialize config iterator", .{});
-            std.process.exit(1);
-        }
-
-        // iterate through config
-        var config_entry: ?*git.git_config_entry = undefined;
-        while (true) {
-            const res = git.git_config_next(&config_entry, iter);
-            if (res < 0) {
-                // means we've read all the aliases
-                if (res == git.GIT_ITEROVER) {
-                    break;
-                }
-
-                std.log.err("error getting next iterator value", .{});
-                std.process.exit(1);
-            }
-
+    // iterate through config
+    var config_iterator = try ConfigIterator.init("^alias\\.");
+    while (config_iterator.next()) |rentry| {
+        if (rentry) |entry| {
             // TODO: don't print suggestions for which val is a subset of
             // another suggestion that has already been/will be given
-            const entry = config_entry.?.*;
             const val = std.mem.span(entry.value);
             if (std.mem.indexOf(u8, args_concat.items, val)) |i| {
                 const name = std.mem.span(entry.name)[6..];
-                try std.io.getStdErr().writer().print("git-brief: you could've used {s} instead of {s}\n",
-                    .{name, args_concat.items[i..i + val.len]});
+                try std.io.getStdErr().writer().print("git-brief: you could've used {s} instead of {s}\n", .{ name, args_concat.items[i .. i + val.len] });
             }
+        } else {
+            break;
         }
-
-        if (git.git_libgit2_shutdown() != 0) {
-            std.log.err("failed to shutdown libgit2", .{});
-            std.process.exit(1);
-        }
+    } else |err| {
+        std.log.err("error getting next iterator value: {}", .{err});
+        std.process.exit(1);
     }
 
-    // useful for checking leaks
-    // _ = gpa.deinit();
+    args_concat.deinit();
+    config_iterator.deinit();
 
-    try argv.append(null);
-    return std.os.execveZ(git_path,
-        @ptrCast([*:null]const ?[*:0]const u8, argv.items),
-        &[_:null]?[*:0]const u8{});
+    // useful for checking leaks; can't be executed in the release version
+    // because argv and git_path cannot be de-allocated before the call to
+    // execveZ, at which point control of the process is transfered to git
+    if (DEBUG_LEAKS) {
+        allocator.destroy(git_path);
+        argv.deinit();
+        _ = gpa.deinit();
+    } else {
+        try argv.append(null);
+        return std.os.execveZ(git_path, @ptrCast([*:null]const ?[*:0]const u8, argv.items), &[_:null]?[*:0]const u8{});
+    }
 }
